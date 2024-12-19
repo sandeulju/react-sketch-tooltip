80e3dd2eadef65893d0e64e1fa7e8f84c76d2b3f
SanDeul <tksemf1706@naver.com>
2024-12-20 03:01:11 +0900

    chore: global.d.ts 생성

---
+++ src/types/global.d.ts
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+
+declare global {
+  interface TooltipProps {
+    content: string;
+    children: ReactNode;
+    position: "top" | "bottom" | "left" | "right";
+    width?: number; // 필수
+    height?: number; // 필수
+    tailWidth?: number; // 선택
+    tailHeight?: number; // 선택
+    cornerCurve?: number; // 선택
+    leftCanvasMargin?: number;
+    topCanvasMargin?: number;
+    styleOptions?: { [key: string]: unknown };
+  }
+
+  interface BubbleSizeType {
+    width: number; // 필수
+    height: number; // 필수
+    tailWidth: number; // 필수
+    tailHeight: number; // 필수
+    cornerCurve: number; // 필수
+    leftCanvasMargin?: number;
+    topCanvasMargin?: number;
+  }
+}
+
+export {};
