0f0d22b07d0b9804953530cfa2c0bb485062cfe3
SanDeul <tksemf1706@naver.com>
2024-12-22 00:31:22 +0900

    fix: TooltipProps 타입 수정 (size 타입으로 묶음)

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -5,13 +5,15 @@ declare global {
     content: string | ReactNode;
     children: ReactNode;
     position: "top" | "bottom" | "left" | "right";
-    width?: number; // 필수
-    height?: number; // 필수
-    tailWidth?: number; // 선택
-    tailHeight?: number; // 선택
-    cornerCurve?: number; // 선택
-    leftCanvasMargin?: number;
-    topCanvasMargin?: number;
+    size: {
+      width?: number; // 필수
+      height?: number; // 필수
+      tailWidth?: number; // 선택
+      tailHeight?: number; // 선택
+      cornerCurve?: number; // 선택
+      leftCanvasMargin?: number;
+      topCanvasMargin?: number;
+    };
     styleOptions?: { [key: string]: unknown };
   }
 
