2faaff7fd5b5e9edfe27c175c574475ab1d7f392
SanDeul <tksemf1706@naver.com>
2024-12-22 22:16:02 +0900

    fix: canvas 초기화 최적화, transition 추가 및 css 수정

--- react-sketch-tooltip/src/components/SketchTooltip.tsx
+++ react-sketch-tooltip/src/components/SketchTooltip.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import rough from "roughjs/bin/rough";
 import {
   createLeftBubble,
@@ -23,11 +23,28 @@ const SketchTooltip: React.FC<TooltipProps> = ({
   styleOptions = {},
 }) => {
   const [showTooltip, setShowTooltip] = useState<boolean>(false);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  const initializeCanvas = (canvas: HTMLCanvasElement | null) => {
+  // 마운트 시 또는 의존성 변경 시 캔버스 초기화
+  useEffect(() => {
+    initializeCanvas();
+  }, [
+    position,
+    styleOptions,
+    width,
+    height,
+    tailWidth,
+    tailHeight,
+    cornerCurve,
+    leftCanvasMargin,
+    topCanvasMargin,
+  ]); // position, styleOptions, size가 변경될 때 재실행
+
+  // 캔버스를 초기화하는 함수
+  const initializeCanvas = () => {
+    const canvas = canvasRef.current;
     if (canvas) {
       const rc = rough.canvas(canvas);
-
       rc.path(
         createBubble(position),
         Object.assign(
@@ -42,6 +59,7 @@ const SketchTooltip: React.FC<TooltipProps> = ({
     }
   };
 
+  // 캔버스를 그리는 경로 생성 함수
   const createBubble = (position: "top" | "bottom" | "left" | "right") => {
     switch (position) {
       case "top":
@@ -88,43 +106,45 @@ const SketchTooltip: React.FC<TooltipProps> = ({
   };
 
   return (
-    <div
-      onMouseEnter={() => setShowTooltip(true)}
-      onMouseLeave={() => setShowTooltip(false)}
-      className="element_wrap"
-    >
-      {children}
-      {showTooltip && (
-        <div className={`tooltip_wrap ${position}`}>
-          <canvas
-            ref={initializeCanvas}
-            width={
-              leftCanvasMargin * 2 +
-              width +
-              cornerCurve * 2 +
-              (position === "left" || position === "right" ? tailHeight : 0)
-            }
-            height={
-              topCanvasMargin * 2 +
-              height +
-              cornerCurve * 2 +
-              (position === "top" || position === "bottom" ? tailHeight : 0)
-            }
-          />
-          <div
-            className="tooltip_content_wrap"
-            style={{
-              top: position === "bottom" ? tailHeight : 0,
-              left: position === "right" ? tailHeight : 0,
-              width: leftCanvasMargin * 2 + width + cornerCurve * 2,
-              height: topCanvasMargin * 2 + height + cornerCurve * 2,
-              padding: cornerCurve / 2,
-            }}
-          >
-            {content}
-          </div>
+    <div className="element_wrap">
+      <div
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+      >
+        {children}
+      </div>
+
+      <div
+        className={`tooltip_wrap ${position} ${showTooltip ? "show active" : "hide"}`}
+      >
+        <canvas
+          ref={canvasRef}
+          width={
+            leftCanvasMargin * 2 +
+            width +
+            cornerCurve * 2 +
+            (position === "left" || position === "right" ? tailHeight : 0)
+          }
+          height={
+            topCanvasMargin * 2 +
+            height +
+            cornerCurve * 2 +
+            (position === "top" || position === "bottom" ? tailHeight : 0)
+          }
+        />
+        <div
+          className="tooltip_content_wrap"
+          style={{
+            top: position === "bottom" ? tailHeight : 0,
+            left: position === "right" ? tailHeight : 0,
+            width: leftCanvasMargin * 2 + width + cornerCurve * 2,
+            height: topCanvasMargin * 2 + height + cornerCurve * 2,
+            padding: cornerCurve / 2,
+          }}
+        >
+          {content}
         </div>
-      )}
+      </div>
     </div>
   );
 };
