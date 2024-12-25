39ae2b0df0a03bb886dc6d2129d3c830b48b5a30
SanDeul <tksemf1706@naver.com>
2024-12-25 21:23:41 +0900

    chore: build를 위해 vite.config.js 수정

--- react-sketch-tooltip/vite.config.ts
+++ react-sketch-tooltip/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "ReactSketchTooltip",
+      formats: ["es", "cjs"],
+      fileName: (format) => `react-sketch-tooltip.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "roughjs"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          roughjs: "roughjs",
+        },
+      },
+    },
+  },
 });
