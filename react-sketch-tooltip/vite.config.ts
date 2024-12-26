240e87c763c133601aeee25dc989bf1c77741a93
SanDeul <tksemf1706@bizwizsystem.co.kr>
2024-12-26 12:00:01 +0900

    chore: vite-plugin-dts 추가

--- react-sketch-tooltip/vite.config.ts
+++ react-sketch-tooltip/vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      // d.ts 파일을 생성할 디렉토리
+      outDir: "dist/types",
+      // tsconfig.json 경로 (자동 탐색도 가능)
+      tsconfigPath: resolve(__dirname, "tsconfig.json"),
+      // 엔트리용 index.d.ts 생성 (package.json "types" 연결)
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
-      entry: "./src/index.ts",
+      entry: resolve(__dirname, "src/index.ts"),
       name: "ReactSketchTooltip",
       formats: ["es", "cjs"],
       fileName: (format) => `react-sketch-tooltip.${format}.js`,
@@ -20,5 +32,6 @@ export default defineConfig({
         },
       },
     },
+    outDir: "dist",
   },
 });
